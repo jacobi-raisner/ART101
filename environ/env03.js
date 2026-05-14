d2716660b1a6da04028cb2f49a96e623d3a6aa8c
jacobi-raisner <jacobicarusoraisner@gmail.com>
2026-05-14 14:59:21 -0700

    en new

--- environ/env03.js
+++ environ/env03.js
@@ -1,12 +1,21 @@
+// ENVIRONMENT DATA
 
 let environmentTitle = "The dark empty arena that is odd";
 
 let environmentMood = "uneasy and echoing";
 
-let environmentSounds = ["weird echoes", "distant bouncing", "whispers in the rafters"];
+let environmentSounds = [
+    "weird echoes",
+    "distant bouncing",
+    "whispers in the rafters"
+];
 
-
-let environmentElements = ["cracked court", "flickering scoreboard", "abandoned bleachers", "hazey blob of mist"];
+let environmentElements = [
+    "cracked court",
+    "flickering scoreboard",
+    "abandoned bleachers",
+    "hazey blob of mist"
+];
 
 let mainEntity = {
     name: "The Ball",
@@ -19,48 +28,70 @@ let mainEntity = {
 let interactionCount = 0;
 
 
+// SHOW TITLE
 
-// show title
 $("#btn-title").click(function () {
-    $("#environment-output").html("<p>" + environmentTitle + "</p>");
+
+    $("#environment-output").html(
+        "<p>" + environmentTitle + "</p>"
+    );
+
 });
 
-// show stuff to person
+
+// SHOW ENVIRONMENT ELEMENTS
+
 $("#btn-elements").click(function () {
 
     let message = "<p>You look around and see:</p>";
 
     for (let i = 0; i < environmentElements.length; i++) {
+
         message = message + "<p>- " + environmentElements[i] + "</p>";
+
     }
 
     $("#environment-output").html(message);
+
 });
 
-// show main entity
+
+// SHOW MAIN ENTITY
+
 $("#btn-entity").click(function () {
 
     let message = "<p>Name: " + mainEntity.name + "</p>";
+
     message = message + "<p>Type: " + mainEntity.type + "</p>";
+
     message = message + "<p>Mood: " + mainEntity.mood + "</p>";
+
     message = message + "<p>Favorite place: " + mainEntity.favoriteElement + "</p>";
 
     $("#environment-output").html(message);
+
 });
 
-// show sounds
+
+// SHOW SOUNDS
+
 $("#btn-sounds").click(function () {
 
     let message = "<p>You listen closely...</p>";
 
     for (let i = 0; i < environmentSounds.length; i++) {
+
         message = message + "<p>" + environmentSounds[i] + "</p>";
+
     }
 
     $("#environment-output").html(message);
+
 });
 
-// interaction counter
+
+// TOUCH THE BALL
+
 $("#btn-touch").click(function () {
 
     interactionCount = interactionCount + 1;
@@ -68,12 +99,90 @@ $("#btn-touch").click(function () {
     let message = "<p>You touched the ball " + interactionCount + " times.</p>";
 
     if (interactionCount > 3) {
+
         message = message + "<p>It feels warmer now.</p>";
+
     }
 
     if (interactionCount > 6) {
+
         message = message + "<p>It is definitely moving on its own.</p>";
+
     }
 
     $("#environment-output").html(message);
+
+});
+
+
+// FUNCTION TO CHANGE ARENA MOOD
+
+function changeArenaMood(arenaMood) {
+
+    // calm state
+    if (arenaMood == "calm") {
+
+        $("body").css({
+            "background-color": "#1a1a1a",
+            "color": "white"
+        });
+
+        $("#arena-output").html(
+            "<p>The whispers fade.</p>" +
+            "<p>The basketball stops moving.</p>"
+        );
+
+    }
+
+    // danger state
+    else if (arenaMood == "danger") {
+
+        $("body").css({
+            "background-color": "#3b1f1f",
+            "color": "white"
+        });
+
+        $("#arena-output").html(
+            "<p>The scoreboard flickers violently.</p>" +
+            "<p>You hear bouncing somewhere in the dark.</p>"
+        );
+
+    }
+
+    // possessed state
+    else {
+
+        $("body").css({
+            "background-color": "black",
+            "color": "red"
+        });
+
+        $("#arena-output").html(
+            "<p>The arena is awake.</p>" +
+            "<p>The ball is staring directly at you.</p>"
+        );
+
+    }
+
+}
+
+
+// BUTTON EVENTS
+
+$("#calm-button").click(function () {
+
+    changeArenaMood("calm");
+
+});
+
+$("#danger-button").click(function () {
+
+    changeArenaMood("danger");
+
+});
+
+$("#possessed-button").click(function () {
+
+    changeArenaMood("possessed");
+
 });
---
+++ garden/creature.js
@@ -0,0 +1,34 @@
+// add creature to the garden
+$("#crAdd").click( function(){
+
+    // grab the value from the text input and assign it to a variable crName
+    let crName=$("#crName").val();
+    let crColor=$("#crColor").val();
+    let crEyesNum=$("#crEyesNum").val();
+
+    // test in console
+    console.log(crName);  
+    console.log(crColor); 
+    console.log(crEyesNum); 
+    
+
+    $("#creature-list").append(`
+            <div class="creature">
+                <div class="creature-body" style="background-color: ${crColor}"> B </div>
+                <div class="creature-info">${crName}</div>
+            </div>
+        `);
+    // lets construct html for eyes
+    let crEyesHtml="";
+    for (let i = 0; i < crEyesNum; i++) {
+        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
+    }
+
+
+   
+// "<div>"+crName+crColor+crEyesNum+"</div>"
+     $("#crName").val(""); // write the value
+    // $("#crName").val(); // retrieve the value
+
+}); 
+
